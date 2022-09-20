6a1da8911badf0308890c14ce5d70ba92d1ce56a
Erdem Karakoylu <ekarakoylu@researchinnovations.com>
2022-09-20 10:43:40 -0400

    removed trainer script

--- trainer.py
+++
@@ -1,7 +0,0 @@
-from pytorch_lightning.utilities.cli import LightningCLI
-from lcf_datamodule import DataModule
-from packages.lcf_pl_model import LCFS_BERT_PL
-
-
-if __name__ == '__main__':
-    cli = LightningCLI(LCFS_BERT_PL, DataModule)
